baf65bbeac06546dea0c93200639c7009a31fbbf
brennschlus <brennschluss@protonmail.com>
2023-12-14 12:24:54 +0300

    deleted my attempt to write logger

--- kernel/src/vga_buffer.rs
+++ kernel/src/vga_buffer.rs
@@ -43,13 +43,3 @@ impl Color {
     }
 }
 
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(transparent)]
-struct ColorCode(u8);
-
-impl ColorCode {
-    fn new(foreground: Color, background: Color) -> ColorCode {
-        ColorCode((background as u8) << 4 | (foreground as u8))
-    }
-}
