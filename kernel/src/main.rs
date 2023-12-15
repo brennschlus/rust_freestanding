0a815daf47158e2297f9d91eff7d16215571f9a7
brennschlus <brennschluss@protonmail.com>
2023-12-15 12:21:47 +0300

    cleaning

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -23,20 +23,18 @@ fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     // free the doubly wrapped framebuffer from the boot info struct
     let frame_buffer_optional = &mut boot_info.framebuffer;
 
-    // free the wrapped framebuffer from the FFI-safe abstraction provided by bootloader_api
-    let frame_buffer_option = frame_buffer_optional.as_mut();
+    // framebuffer from the FFI-safe abstraction provided by bootloader_api
+    let frame_buffer = frame_buffer_optional.as_mut().unwrap();
 
-    // unwrap the framebuffer
-    let frame_buffer_struct = frame_buffer_option.unwrap();
 
     // extract the framebuffer info and, to satisfy the borrow checker, clone it
-    let frame_buffer_info = frame_buffer_struct.info().clone();
+    let frame_buffer_info = frame_buffer.info();
 
     let background_color = Color::LightRed;
 
-    background_paint(frame_buffer_struct, background_color);
+    background_paint(frame_buffer, background_color);
 
-    let raw_frame_buffer = frame_buffer_struct.buffer_mut();
+    let raw_frame_buffer = frame_buffer.buffer_mut();
 
     init_logger(raw_frame_buffer, frame_buffer_info);
 
